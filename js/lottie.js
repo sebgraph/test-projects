eded45aae12a73fbd48885b2f86dd0f3164e2f2d
sebgraph <72751773+sebgraph@users.noreply.github.com>
2021-01-13 19:10:06 -0500

    Add files via upload

--- js/lottie.js
+++ js/lottie.js
@@ -1,10 +1,67 @@
 // Animación iconos
-const animationIconosJuega = bodymovin.loadAnimation({
-    container: document.getElementById('icono-juega'),
-    renderer: 'svg',
-    loop: 2,
-    autoplay: true,
-    path: './assets/icono_juega.json'
-});
+
+const iconoJuega = document.getElementsByClassName("icono-juega"),
+      iconoAprende = document.getElementsByClassName("icono-aprende"),
+      iconoPrueba = document.getElementsByClassName("icono-prueba");            
 
 
+function animacionJuega(iconoJuega){
+    const animation = bodymovin.loadAnimation({
+        container: iconoJuega,
+        renderer: 'svg',
+        loop: 2,
+        autoplay: true,
+        path: './assets/icono_juega.json'
+        });
+
+    iconoJuega.addEventListener("mouseenter", function() {
+        animation.play();
+      });
+      iconoJuega.addEventListener("mouseleave", function() {
+        animation.stop();
+      });
+     }
+
+function animacionAprende(iconoAprende){
+        const animation = bodymovin.loadAnimation({
+            container: iconoAprende,
+            renderer: 'svg',
+            loop: 2,
+            autoplay: true,
+            path: './assets/icono_aprende.json'
+            });
+        iconoAprende.addEventListener("mouseenter", function() {
+            animation.play();
+          });
+          iconoAprende.addEventListener("mouseleave", function() {
+            animation.stop();
+          });
+    }
+
+function animacionPrueba(iconoPrueba){
+        const animation = bodymovin.loadAnimation({
+            container: iconoPrueba,
+            renderer: 'svg',
+            loop: 2,
+            autoplay: true,
+            path: './assets/icono_prueba.json'
+            });
+        iconoPrueba.addEventListener("mouseenter", function() {
+            animation.play();
+          });
+          iconoPrueba.addEventListener("mouseleave", function() {
+            animation.stop();
+          });
+    }
+
+
+
+     for (let i = 0; i < iconoJuega.length; i++) {
+     animacionJuega(iconoJuega[i]);
+     animacionAprende(iconoAprende[i]);
+     animacionPrueba(iconoPrueba[i]);
+     }
+
+
+
+      
